dcd3e3a58561eb6a939793cbbba95cb643333561
mfadzilr <mind1355@gmail.com>
2017-02-10 00:29:31 +0800

    adding ajax host details update

--- app/controllers/affected_hosts_controller.rb
+++ app/controllers/affected_hosts_controller.rb
@@ -1,9 +1,12 @@
 class AffectedHostsController < ApplicationController
+  before_action :set_affected_host, only: [:edit, :update]
+
   def index
     @affected_hosts = AffectedHost.where(source_file_id: params[:source_id])
   end
 
   def show
+
   end
 
   def create
@@ -13,6 +16,18 @@ class AffectedHostsController < ApplicationController
   end
 
   def update
+    def update
+      respond_to do |format|
+        if @affected_host.update(affected_host_params)
+          format.html { redirect_to @affected_host, notice: 'Affected host was successfully updated.' }
+          format.json { render :show, status: :ok, location: @affected_host }
+          format.js
+        else
+          format.html { render :edit }
+          format.json { render json: @affected_host.errors, status: :unprocessable_entity }
+        end
+      end
+    end
   end
 
   def destroy
@@ -20,8 +35,16 @@ class AffectedHostsController < ApplicationController
 
   private
 
-  def source_file_params
-    params.fetch(:affected_hosts, {}).permit(:source_id)
+  def set_affected_host
+    @affected_host = AffectedHost.find(params[:id])
+  end
+
+  # def source_file_params
+  #   params.fetch(:affected_hosts, {}).permit(:source_id)
+  # end
+
+  def affected_host_params
+    params.fetch(:affected_host, {}).permit(:host_fqdn, :netbios_name, :mac_address, :os)
   end
 
 end
