65dcfb8d77d6a4a9e6d585f4c8e6dae1235e82b8
mfadzilr <mind1355@gmail.com>
2017-02-13 17:11:43 +0800

    added pie & radar chart

--- app/helpers/source_files_helper.rb
+++ app/helpers/source_files_helper.rb
@@ -1,29 +1,22 @@
 module SourceFilesHelper
-  # def self.write_to_db(nessus_data)
-  #   doc = Nokogiri::XML(nessus_data)
-  #   doc.xpath('/NessusClientData_v2/Report').each do |report|
-  #     report.xpath('./ReportHost').each do |host|
-  #       @hosts = AffectedHost.new
-  #       host.xpath('./HostProperties/tag').each do |tag|
-  #         if tag.attributes['name'].value == 'host-ip'
-  #           @hosts.host_ip = tag.text
-  #         elsif tag.attributes['name'].value == 'host-fqdn'
-  #           @hosts.host_fqdn = tag.text
-  #         elsif tag.attributes['name'].value == 'netbios-name'
-  #           @hosts.netbios_name = tag.text
-  #         elsif tag.attributes['name'].value == 'mac-address'
-  #           @hosts.mac_address = tag.text
-  #         elsif tag.attributes['name'].value == 'operating-system'
-  #           @hosts.os = tag.text
-  #         elsif tag.attributes['name'].value == 'HOST_START'
-  #           @hosts.scan_start = Time.parse(tag.text).strftime("%Y-%m-%d %I:%M:%S")
-  #         elsif tag.attributes['name'].value == 'HOST_END'
-  #           @hosts.scan_end = Time.parse(tag.text).strftime("%Y-%m-%d %I:%M:%S")
-  #         end
-  #       end
-  #       @hosts.upload_id = this.id
-  #       @hosts.save
-  #     end
-  #   end
-  # end
+
+  def get_pie_data
+    @severity_data =  @affected_hosts.joins(:vulnerabilities).group(:severity).count.values
+
+    @data = {
+      labels: ["Critical", "High", "Medium", "Low"],
+      datasets: [
+        {
+          data: @severity_data,
+          backgroundColor: [
+            '#CC0000',
+            '#FF8800',
+            '#FFFF00',
+            '#007E33'
+          ]
+        }
+      ]
+    }
+  end
+
 end
