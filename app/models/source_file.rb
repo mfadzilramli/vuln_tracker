237a2c3fdb08370d1df3d2c8b987bc744c6a7239
mfadzilr <mind1355@gmail.com>
2017-03-06 08:44:36 +0800

    add error msg alertbox

--- app/models/source_file.rb
+++ app/models/source_file.rb
@@ -1,6 +1,6 @@
 class SourceFile < ApplicationRecord
-  validates :title, presence: true
-  
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
+
   has_many  :affected_hosts, dependent: :destroy
   has_many :monitored_items, dependent: :destroy
   has_many :project_groups, through: :monitored_items
